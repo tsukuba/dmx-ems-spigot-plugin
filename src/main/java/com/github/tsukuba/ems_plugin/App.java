6973508dff372fc7fe5547bfc7cad1991f85e278
pepepper <hollyholly2014@outlook.jp>
2022-09-09 01:08:33 +0900

    remove URL message

--- src/main/java/com/github/tsukuba/ems_plugin/App.java
+++ src/main/java/com/github/tsukuba/ems_plugin/App.java
@@ -2,11 +2,6 @@ package com.github.tsukuba.ems_plugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import org.bukkit.configuration.file.FileConfiguration;
-
-import net.md_5.bungee.api.chat.*;
-import net.md_5.bungee.api.ChatColor;
-
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -14,51 +9,19 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
-import org.bukkit.event.player.*;
 
 import java.nio.charset.StandardCharsets;
-import java.util.Base64;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.URI;
 
 public class App extends JavaPlugin implements Listener {
-    private String eventserver_url;
-    private String frontserver_url;
 
     @Override
     public void onEnable() {
         getLogger().info("Spark, spark, spark!");
         getServer().getPluginManager().registerEvents(this, this);
-        getConfig().options().copyDefaults(true);
-        saveDefaultConfig();
-        FileConfiguration config = getConfig();
-        eventserver_url = config.getString("event_server");
-        frontserver_url = config.getString("front_server");
-    }
-
-    @EventHandler
-    public void PlayerJoinEvent(PlayerJoinEvent event) {
-        Player p = event.getPlayer();
-        String setting_json = "{\"server\":\"" + eventserver_url + "\",\"id\":\"" + p.getName() + "\"}";
-        TextComponent url = new TextComponent(ChatColor.BLUE + "このURL");
-        ComponentBuilder cb = new ComponentBuilder(
-                "URL").bold(true).color(ChatColor.BLUE)
-                .append(frontserver_url).color(ChatColor.GREEN).bold(false);
-        url.setColor(ChatColor.AQUA);
-        url.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, cb.create()));
-        url.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, frontserver_url + "?setting=" +
-                new String(
-                        Base64
-                                .getUrlEncoder()
-                                .withoutPadding()
-                                .encode(
-                                        setting_json
-                                                .getBytes(StandardCharsets.US_ASCII)),
-                        StandardCharsets.US_ASCII)));
-        url.addExtra(new TextComponent(ChatColor.WHITE + "に接続し感電マシーンの設定を行ってください"));
-        p.spigot().sendMessage(url);
     }
 
     private void event_sender(String player_id, int modifier) {
